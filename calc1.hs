b4c2c4de43a8dae41924c5dc2d3e7d9340eb48ab
Ramesh Baskaran <ramesh.baskaran@gmail.com>
2025-11-25 12:09:46 +0530

    Fix syntax errors in calc1.hs

--- calc1.hs
+++ calc1.hs
@@ -1,16 +1,14 @@
 module Main where
 
-main :: IO ()
-main = do
-    geometricSeriesSum :: Double -> Double -> Int -> Double
+geometricSeriesSum :: Double -> Double -> Int -> Double
 geometricSeriesSum a r n
   | r == 1.0  = a * fromIntegral n
   | otherwise = a * (1.0 - r**fromIntegral n) / (1.0 - r)
 
-
- 
-  putStrLn $ "The sum of the geometric series is: " ++ show geometricSeriesSum 5 5 5
-  putStrLn $ "The sum of the geometric series is: " ++ show geometricSeriesSum 10 10 10
-  putStrLn $ "The sum of the geometric series is: " ++ show geometricSeriesSum 24 24 24 
-  putStrLn $ "The sum of the geometric series is: " ++ show geometricSeriesSum 26 26 26
+main :: IO ()
+main = do
+  putStrLn $ "The sum of the geometric series is: " ++ show (geometricSeriesSum 5 5 5)
+  putStrLn $ "The sum of the geometric series is: " ++ show (geometricSeriesSum 10 10 10)
+  putStrLn $ "The sum of the geometric series is: " ++ show (geometricSeriesSum 24 24 24)
+  putStrLn $ "The sum of the geometric series is: " ++ show (geometricSeriesSum 26 26 26)
 
